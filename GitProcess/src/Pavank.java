1f732d912ae6a75266537cb8f7d0b1e9a2e055ac
ADMIN <ADMIN@DESKTOP-RJ8F2SK>
2022-09-15 11:28:48 +0530

    second commit

--- GitProcess/src/Pavank.java
+++ GitProcess/src/Pavank.java
@@ -4,6 +4,8 @@ public class Pavank {
 	public static void main(String[] args) {
 		
       System.out.println("PavanKalyan");
+      System.out.println("PavanKalyan");
+      System.out.println("PavanKalyan");
 	}
 
 }
